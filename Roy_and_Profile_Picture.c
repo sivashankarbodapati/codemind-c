0a1239dc6687894a4f48b1ea3e02b4871b979e86
sivashankarbodapati <119917360+sivashankarbodapati@users.noreply.github.com>
2023-01-22 10:17:43 +0530

    #include<stdio.h> int main() {     int l,n,w,h,i;     scanf("%d",&l);     scanf("%d",&n);     for(i=1;i<=n;i++) {         scanf("%d%d",&w,&h);         if (w<l || h<l) {         printf("UPLOAD ANOTHER ");         }         else if (w==h) {             printf("ACCEPTED ");         }         else {             printf("CROP IT ");         }     } }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,w,h,i;
+    scanf("%d",&l);
+    scanf("%d",&n);
+    for(i=1;i<=n;i++) {
+        scanf("%d%d",&w,&h);
+        if (w<l || h<l) {
+        printf("UPLOAD ANOTHER
+");
+        }
+        else if (w==h) {
+            printf("ACCEPTED
+");
+        }
+        else {
+            printf("CROP IT
+");
+        }
+    }
+}
