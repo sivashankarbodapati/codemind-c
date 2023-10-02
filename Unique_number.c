865292e1a14d59eaf94fb698b33baf84686cfd9b
sivashankarbodapati <119917360+sivashankarbodapati@users.noreply.github.com>
2023-10-02 19:36:09 +0530

    #include<stdio.h> int main() {     int n,z=0,o=0,t=0,th=0,f=0,fi=0,s=0,se=0,e=0,ni=0;     scanf("%d",&n);     while(n)     {         if(n%10==0)            z++;         else if(n%10==1)            o++;         else if(n%10==2)           t++;         else if(n%10==3)           th++;         else if(n%10==4)           f++;         else if(n%10==5)           fi++;         else if(n%10==6)           s++;         else if(n%10==7)           se++;         else if(n%10==8)           e++;         else if(n%10==9)           ni++;           n/=10;     }     if(z>1 || o>1 || t>1 || th>1 || f>1 || fi>1 || s>1 || se>1 || e>1 || ni>1)     {         printf("Not Unique Number");     }     else     {         printf("Unique Number");     } }

--- Unique_number.c
+++ Unique_number.c
@@ -1,38 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    int n,a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0;
+    int n,z=0,o=0,t=0,th=0,f=0,fi=0,s=0,se=0,e=0,ni=0;
     scanf("%d",&n);
     while(n)
     {
         if(n%10==0)
-             a++;
+           z++;
         else if(n%10==1)
-             b++;
-          else if(n%10==2)
-             c++;
-          else if(n%10==3)
-              d++;
-            else if(n%10==4)
-             e++;
-          else if(n%10==5)
-             f++;
-          else if(n%10==6)
-             g++;
-          else if(n%10==7)
-             h++;
-          else if(n%10==8)
-             i++;
-          else if(n%10==9)
-             j++;
-             n/=10;
-         }
-    if(a>1 || b>1 || c>1 || d>1 || e>1 || f>1 || g>1 || h>1 || i>1 || j>1)
-         {
+           o++;
+        else if(n%10==2)
+          t++;
+        else if(n%10==3)
+          th++;
+        else if(n%10==4)
+          f++;
+        else if(n%10==5)
+          fi++;
+        else if(n%10==6)
+          s++;
+        else if(n%10==7)
+          se++;
+        else if(n%10==8)
+          e++;
+        else if(n%10==9)
+          ni++;
+          n/=10;
+    }
+    if(z>1 || o>1 || t>1 || th>1 || f>1 || fi>1 || s>1 || se>1 || e>1 || ni>1)
+    {
         printf("Not Unique Number");
-         }
-        else
-        {
-         printf("Unique Number");   
-        }
+    }
+    else
+    {
+        printf("Unique Number");
+    }
 }
