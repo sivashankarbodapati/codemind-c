5deb356cb1d0ad7f8d478716852f84afce550106
sivashankarbodapati <119917360+sivashankarbodapati@users.noreply.github.com>
2023-10-22 17:36:50 +0530

    #include<stdio.h> int main() {     int n,x=0;     scanf("%d",&n);     if(n<=0)     {         //printf("Input a correct number.");     }     while(n!=1)     {         if(n%5==0)         {             n/=5;         }         else if(n%3==0)         {             n/=3;         }         else if(n%2==0)         {             n/=2;         }         else         {             printf("Not Ugly Number ");             x=1;             break;         }     }     if(x==0)     {         printf("Ugly Number ");     } }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,31 +1,37 @@
 #include<stdio.h>
 int main()
 {
-    int n,count=0;
+    int n,x=0;
     scanf("%d",&n);
+    if(n<=0)
+    {
+        //printf("Input a correct number.");
+    }
     while(n!=1)
     {
-        if(n%2==0)
+        if(n%5==0)
         {
-            n/=2;
+            n/=5;
         }
         else if(n%3==0)
         {
             n/=3;
         }
-        else if(n%5==0)
+        else if(n%2==0)
         {
-            n/=5;
+            n/=2;
         }
         else
         {
-            printf("Not Ugly Number");
-            count=1;
+            printf("Not Ugly Number
+");
+            x=1;
             break;
         }
     }
-    if(count==0)
+    if(x==0)
     {
-        printf("Ugly Number");
+        printf("Ugly Number
+");
     }
 }
